b00708360f65bee41b74719962a1d0023853c333
Your Name <you@example.com>
2025-11-05 15:00:27 +0600

    d

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,13 +1,15 @@
-// src/user/user.module.ts
+// src/admin/admin.module.ts
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { UserAgentSelectionByEmailController, UserAgentSelectionByIdController } from './admin.controller';
+import {
+  UserAgentSelectionByEmailController,
+  UserAgentSelectionByIdController,
+} from './admin.controller';
 import { UserAgentSelectionService } from './admin.service';
-
 
 @Module({
   controllers: [UserAgentSelectionByIdController, UserAgentSelectionByEmailController],
   providers: [PrismaService, UserAgentSelectionService],
   exports: [UserAgentSelectionService],
 })
-export class UserModule {}
+export class AdminModule {}
